996cfba4880d8bd9c0dfb4c73a42dbab4e7a4c9e
Rakesh1186 <rakesh.tashildar@trustt.com>
2024-05-24 09:50:52 +0530

    changed to sales history table

--- App.js
+++ App.js
@@ -1,190 +1,314 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, PermissionsAndroid, Dimensions } from 'react-native';
-import React, { useEffect } from 'react';
-import { BarCodeScanner } from 'expo-barcode-scanner';
-import { ScrollView, TextInput, FlatList } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
-import * as FileSystem from 'expo-file-system';
-import XLSX from 'xlsx';
-import * as Sharing from 'expo-sharing';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import React from "react";
+import { ScrollView, TextInput, FlatList } from "react-native";
+import * as FileSystem from "expo-file-system";
+import XLSX from "xlsx";
+import * as Sharing from "expo-sharing";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { Picker } from "@react-native-picker/picker";
 
 export default function App() {
-  const [item, setItem] = React.useState('');
-  const [quantity, setQuantity] = React.useState('');
-  const [amount, setAmount] = React.useState('');
-  const [paymentMode, setPaymentMode] = React.useState('');
-  const [date, setDate] = React.useState('');
+  const [item, setItem] = React.useState("");
+  const [quantity, setQuantity] = React.useState("");
+  const [amount, setAmount] = React.useState("");
+  const [paymentMode, setPaymentMode] = React.useState("Cash");
+  const [date, setDate] = React.useState(new Date());
+  const [showPicker, setShowPicker] = React.useState(false);
+
+  const toggleDatePicker = () => {
+    setShowPicker(!showPicker);
+  };
+
+  const onChange = ({ type }, selectedDate) => {
+    if (type === "set") {
+      const currentDate =
+        typeof selectedDate === "object" ? selectedDate : date;
+      console.log(currentDate.toString());
+      setDate(currentDate);
+    } else {
+      toggleDatePicker();
+    }
+    setShowPicker(false);
+  };
 
   const [data, setData] = React.useState([]);
 
   const handleSubmit = () => {
     const entry = {
-      date: date,
+      date: formatDate(date),
       item: item,
       quantity: quantity,
       amount: amount,
       paymentMode: paymentMode,
-      id: data.length.toString()
+      id: data.length.toString(),
     };
-    setData(prevData => [...prevData, entry]);
-    setDate('');
-    setItem('');
-    setQuantity('');
-    setAmount('');
-    setPaymentMode('');
-    console.log(data);
+    setData((prevData) => [...prevData, entry]);
+    setDate(new Date());
+    setItem("");
+    setQuantity("");
+    setAmount("");
+    setPaymentMode("Cash");
   };
 
-  const genrateExcel = async () => {
+  const generateExcel = async () => {
+    if (data.length > 0) {
+      let wb = XLSX.utils.book_new();
+      console.log(wb);
 
-    let wb = XLSX.utils.book_new();
-    console.log(wb);
+      const aoaData = [csvHeader].concat(
+        data.map((item, index) => [
+          index + 1,
+          item.date,
+          item.item,
+          item.quantity,
+          item.amount,
+          item.paymentMode,
+        ])
+      );
+      const totalAmount = data.reduce(
+        (sum, item) => sum + parseFloat(item.amount),
+        0
+      );
+      aoaData.push(["Total Amount", "", "", "", totalAmount.toFixed(2), ""]);
+      let ws = XLSX.utils.aoa_to_sheet(aoaData);
+      XLSX.utils.book_append_sheet(wb, ws, "Sales_history", true);
 
-
-    const aoaData = [csvHeader].concat(data.map(item => [item.date, item.item, item.quantity, item.amount, item.paymentMode]));
-    console.log(aoaData);
-    let ws = XLSX.utils.aoa_to_sheet(aoaData);
-    XLSX.utils.book_append_sheet(wb, ws, 'Sales_history', true);
-
-    const base64 = XLSX.write(wb, { type: 'base64', bookType: 'xlsx' });
-    const fileName = FileSystem.documentDirectory + 'Sales_historyFirst.xlsx';
-    FileSystem.writeAsStringAsync(fileName, base64, { encoding: FileSystem.EncodingType.Base64 })
-      .then(() => {
-        console.log('Excel file has been written successfully!' + fileName);
-        Sharing.shareAsync(fileName);
+      const base64 = XLSX.write(wb, { type: "base64", bookType: "xlsx" });
+      const currentDate = new Date();
+      const day = String(currentDate.getDate()).padStart(2, "0");
+      const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+      const year = String(currentDate.getFullYear()).slice(-2);
+      const hours = String(currentDate.getHours()).padStart(2, "0");
+      const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+      const seconds = String(currentDate.getSeconds()).padStart(2, "0");
+      const fileName =
+        FileSystem.documentDirectory +
+        "Sales_historyFirst_" +
+        day +
+        "-" +
+        month +
+        "-" +
+        year +
+        "_" +
+        hours +
+        "-" +
+        minutes +
+        "-" +
+        seconds +
+        ".xlsx";
+      FileSystem.writeAsStringAsync(fileName, base64, {
+        encoding: FileSystem.EncodingType.Base64,
       })
-      .catch(error => {
-        console.error('An error occurred while writing the Excel file:', error);
-      });
-  }
-  const renderItem = ({ item }) => (
+        .then(() => {
+          console.log("Excel file has been written successfully!" + fileName);
+          Sharing.shareAsync(fileName);
+        })
+        .catch((error) => {
+          console.error(
+            "An error occurred while writing the Excel file:",
+            error
+          );
+        });
+    } else {
+      alert("No data entered. File not created.");
+    }
+  };
+  const handleRemoveItem = (index) => {
+    setData((prevData) => {
+      const newData = [...prevData];
+      newData.splice(index, 1);
+      return newData;
+    });
+  };
+  const renderItem = ({ item, index }) => (
     <View style={styles.row}>
+      <Text style={styles.cell}>{index + 1}</Text>
       <Text style={styles.cell}>{item.date}</Text>
       <Text style={styles.cell}>{item.item}</Text>
       <Text style={styles.cell}>{item.quantity}</Text>
       <Text style={styles.cell}>{item.amount}</Text>
       <Text style={styles.cell}>{item.paymentMode}</Text>
+      <TouchableOpacity onPress={() => handleRemoveItem(index)}>
+        <Text style={styles.removeButton}>x</Text>
+      </TouchableOpacity>
     </View>
-  )
-  const csvHeader = ['Date', 'Item', 'Quantity', 'Amount', 'Payment Mode'];
+  );
+  const csvHeader = [
+    "Index",
+    "Date",
+    "Item",
+    "Quantity",
+    "Amount",
+    "Payment Mode",
+  ];
+  const formatDate = (date) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+
+    // Check if date is a Date object
+    if (!(date instanceof Date)) {
+      // If not, try to convert it to a Date object
+      date = new Date(date);
+    }
+
+    // Check if date is a valid Date object
+    if (isNaN(date.getTime())) {
+      // If not, return an error message or handle the error in some other way
+      return "Invalid date";
+    }
+
+    return date.toLocaleDateString(undefined, options);
+  };
 
   return (
     <View style={styles.container}>
-      <ScrollView >
-        <View style={{}}>
-          <TextInput
-            style={styles.input}
-            placeholder="Date"
-            value={date}
-            onChangeText={setDate}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Item"
-            value={item}
-            onChangeText={setItem}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Quantity"
-            value={quantity}
-            onChangeText={setQuantity}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Amount"
-            value={amount}
-            onChangeText={setAmount}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Payment Mode"
-            value={paymentMode}
-            onChangeText={setPaymentMode}
-          />
-
-          <Button title="Submit" onPress={handleSubmit} />
-        </View>
-        <View style={styles.header}>
-          <Text style={styles.heading}>Date</Text>
-          <Text style={styles.heading}>Item</Text>
-          <Text style={styles.heading}>Quantity</Text>
-          <Text style={styles.heading}>Amount</Text>
-          <Text style={styles.heading}>Payment Mode</Text>
-        </View>
+      <TouchableOpacity
+        onPress={() => setShowPicker(true)}
+        style={styles.dateContainer}
+      >
+        <Text>{date ? formatDate(date) : formatDate(new Date())}</Text>
+      </TouchableOpacity>
+      {showPicker && (
+        <DateTimePicker
+          value={date || new Date()}
+          onChange={onChange}
+          mode="date"
+        />
+      )}
+      <TextInput
+        style={styles.input}
+        placeholder="Item"
+        value={item}
+        onChangeText={setItem}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Quantity"
+        value={quantity}
+        onChangeText={(text) => setQuantity(text === "" ? "0" : text)}
+        keyboardType="numeric" // Added keyboardType prop
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Amount"
+        value={amount}
+        onChangeText={(text) => setAmount(text === "" ? "0" : text)}
+        keyboardType="numeric" // Added keyboardType prop
+      />
+      <Picker
+        selectedValue={paymentMode}
+        onValueChange={(itemValue) => setPaymentMode(itemValue)}
+      >
+        <Picker.Item label="Cash" value="Cash" />
+        <Picker.Item label="UPI" value="UPI" />
+      </Picker>
+      <TouchableOpacity
+        style={{ marginTop: "5" }}
+        title="Submit"
+        onPress={handleSubmit}
+      >
+        <Text style={styles.exportButton}>Submit</Text>
+      </TouchableOpacity>
+      <View style={styles.header}>
+        <Text style={styles.heading}>SL No</Text>
+        <Text style={styles.heading}>Date</Text>
+        <Text style={styles.heading}>Item</Text>
+        <Text style={styles.heading}>Quantity</Text>
+        <Text style={styles.heading}>Amount</Text>
+        <Text style={styles.heading}>Payment Mode</Text>
+      </View>
+      <ScrollView>
         <FlatList
           data={data}
           keyExtractor={(item) => item.id.toString()}
           renderItem={renderItem}
-          style={{ backgroundColor: '#BEBEBE', margin: '1px' }}>
-        </FlatList>
-        <Button style={styles.exportButton} title="Export Data" onPress={genrateExcel} />
+        ></FlatList>
       </ScrollView>
-
+      <TouchableOpacity
+        style={{ position: "fixed", bottom: "0", color: "red" }}
+        title="Export Data"
+        onPress={generateExcel}
+      >
+        <Text style={styles.exportButton}>Export Data</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-
-
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingVertical: 30,
-    paddingHorizontal: 30,
-    marginTop: 50,
-    height: Dimensions.get('window').height,
-  },
-  headerToBar: {
-    backgroundColor: 'blue',
-    paddingHorizontal: 12,
-    paddingVertical: 10,
-    borderRadius: 10,
-    elevation: 2,
-    marginBottom: 10,
-  },
-  input: {
-    height: '10%',
-    width: '100%',
-    borderColor: 'gray',
+  dateContainer: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "flex-start",
     borderWidth: 1,
-    marginBottom: 10,
+    borderColor: "lightskyblue",
+    borderRadius: 5,
+    paddingRight: 10,
+    height: "5%",
+    marginBottom: 3,
+    width: "100%",
     paddingHorizontal: 10,
   },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "white", // Modified background color
+    paddingVertical: 3,
+    paddingHorizontal: 5,
+    height: "100%",
+    marginTop: 70,
+  },
+  input: {
+    height: "5%",
+    width: "100%",
+    borderColor: "lightskyblue",
+    borderWidth: 1,
+    marginBottom: 3,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginVertical: 10,
-    marginHorizontal: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
     elevation: 2,
     borderRadius: 3,
-    borderColor: 'gray',
+    borderColor: "gray",
     border: 10,
-    backgroundColor: 'white',
-
+    backgroundColor: "lightyellow",
   },
   cell: {
-    fontSize: 15,
-    fontFamily: 'sans-serif',
-    textAlign: 'left',
+    fontSize: 10,
+    fontFamily: "sans-serif",
+    textAlign: "center",
     flex: 1,
+    textAlignVertical: "center",
+    borderColor: "seashell",
+    borderWidth: 1,
   },
   heading: {
     flex: 1,
-    fontFamily: 'sans-serif',
+    fontFamily: "sans-serif",
     fontSize: 10,
   },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
     padding: 10,
   },
+  removeButton: {
+    width: 20,
+    height: 20,
+    color: "red",
+    textAlign: "center",
+    backgroundColor: "black",
+    borderRadius: 10,
+  },
   exportButton: {
-    position: 'fixed',
-    bottom: 0,
-    height: '10%',
-    width: '100%',
+    textAlignVertical: "center",
+    alignSelf: "center",
+    width: "50%",
+    height: 30,
+    textAlign: "center",
+    backgroundColor: "tomato",
+    marginBottom: 10,
+    borderRadius: 5,
   },
 });
